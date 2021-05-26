bd3a38c9cd220aea3fec67ef0482ddb37ff2020a
Yors <jatbribiesca@gmail.com>
2021-05-26 11:27:24 -0500

    add spinner

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -12,26 +12,37 @@ import {
 
 import { updateCollections } from '../../redux/shop/shop.actions.js';
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends React.Component {
+	state = {
+		loading: true
+	};
+
 	unsuscribeFromSnapshot = null;
 
 	componentDidMount() {
-    const { updateCollections } = this.props
+		const { updateCollections } = this.props;
 		const collectionRef = firestore.collection('collections');
 		collectionRef.onSnapshot(snapshot => {
 			const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
 			updateCollections(collectionsMap);
+			this.setState({ loading: false });
 		});
 	}
 
 	render() {
 		const { match } = this.props;
+    const { loading } = this.state;
 		return (
 			<div className='shop-page'>
-				<Route exact path={`${match.path}`} component={CollectionsOverview} />
+				<Route exact path={`${match.path}`} render={(props) => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />
 				<Route
 					path={`${match.path}/:collectionId`}
-					component={CollectionPage}
+					render={(props) => <CollectionPageWithSpinner isLoading={loading} {...props} />}
 				/>
 			</div>
 		);
