46eae13c9885bdc8743310935aeb32ed2a2a937b
Yors <jatbribiesca@gmail.com>
2021-05-26 18:26:46 -0500

    install redux thunk create containers for collection overview and collection page

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -8,13 +8,24 @@ export const selectCollections = createSelector(
 	shop => shop.collections
 );
 
-export const selectCollectionsForPreview =  createSelector(
-  [selectCollections],
-  collections => collections ? Object.keys(collections).map(key => collections[key]) : []
-)
+export const selectCollectionsForPreview = createSelector(
+	[selectCollections],
+	collections =>
+		collections ? Object.keys(collections).map(key => collections[key]) : []
+);
 
 export const selectCollection = memoize(collectionUrlParam =>
 	createSelector([selectCollections], collections =>
-		(collections ? collections[collectionUrlParam] : null)
+		collections ? collections[collectionUrlParam] : null
 	)
+);
+
+export const selectIsCollectionFetching = createSelector(
+	[selectShop],
+	shop => shop.isFetching
+);
+
+export const selectIsCollectionsLoaded = createSelector(
+	[selectShop],
+	shop => !!shop.collections
 );
